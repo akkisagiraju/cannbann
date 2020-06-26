dedf7e04849c555e207d411ea93a91a01ad9652e
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-06-26 08:52:31 +0530

    refactored auth router

--- server/src/app.js
+++ server/src/app.js
@@ -5,8 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./utils/config');
 const middleware = require('./utils/middleware');
-const userRouter = require('./controllers/user');
-const loginRouter = require('./controllers/login');
+const authRouter = require('./controllers/auth');
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
@@ -17,9 +16,9 @@ mongoose.connect(config.MONGODB_URI, {
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', userRouter);
-app.use('/login', loginRouter);
+app.use('/auth', authRouter);
 
 app.use(middleware.errorHandler);
+app.use(middleware.tokenExtractor);
 
 module.exports = app;
--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,12 +1,25 @@
 /* eslint-disable operator-linebreak */
 /* eslint-disable no-underscore-dangle */
 
-const loginRouter = require('express').Router();
+const authRouter = require('express').Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+require('express-async-errors');
 const User = require('../models/User');
 
-loginRouter.post('/', async (request, response) => {
+authRouter.post('/signup', async (request, response) => {
+  const { name, email, password } = request.body;
+
+  const SALT_ROUNDS = 10;
+  const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+
+  const user = new User({ name, email, passwordHash });
+  await user.save();
+
+  response.status(200).send({ message: 'User signed up successfully' });
+});
+
+authRouter.post('/signin', async (request, response) => {
   const { email, password } = request.body;
 
   // check if the user is in db using the email
@@ -29,4 +42,4 @@ loginRouter.post('/', async (request, response) => {
   response.status(200).send({ token, name: user.name, email: user.email });
 });
 
-module.exports = loginRouter;
+module.exports = authRouter;
