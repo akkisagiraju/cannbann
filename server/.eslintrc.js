30a5ea9b74d83c1fa25f19418745895449e517e1
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-06-26 07:17:56 +0530

    added jest and a few rules

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
+    jest: true
   },
   extends: [
     'airbnb-base',
@@ -25,6 +26,12 @@ module.exports = {
     'node/prefer-global/url-search-params': ['error', 'always'],
     'node/prefer-global/url': ['error', 'always'],
     'node/prefer-promises/dns': 'error',
-    'node/prefer-promises/fs': 'error'
+    'node/prefer-promises/fs': 'error',
+    'comma-dangle': 0,
+    'no-console': 0,
+    'no-unused-vars': [
+      1,
+      { vars: 'all', args: 'none', ignoreRestSiblings: false }
+    ]
   }
 };
