6b71e1100f45b719e6191b8e5b300646788d6b07
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-06-27 12:10:45 +0530

    added password length checker

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -9,11 +9,16 @@ const User = require('../models/User');
 
 authRouter.post('/signup', async (request, response) => {
   const { name, email, password } = request.body;
+  if (password.length < 3) {
+    return response
+      .status(400)
+      .send({ error: 'Password has to be more than 3 characters' });
+  }
   const passwordHash = await generatePasswordHash(password);
   const user = new User({ name, email, passwordHash });
   await user.save();
 
-  response.status(200).send({ message: 'User signed up successfully' });
+  return response.status(200).send({ message: 'User signed up successfully' });
 });
 
 authRouter.post('/signin', async (request, response) => {
