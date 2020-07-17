8b569551cc3e9915563dba32dbedd133dbd89385
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-07-17 22:13:16 +0530

    removed the setting of user token

--- client/src/config/axios.ts
+++ client/src/config/axios.ts
@@ -2,19 +2,4 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:5000';
 
-let user;
-
-try {
-  user = JSON.parse(window.localStorage.getItem('user') as string);
-  axios.defaults.headers.common = {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${user.token}`
-  };
-} catch (error) {
-  user = {};
-  axios.defaults.headers.common = {
-    'Content-Type': 'application/json'
-  };
-}
-
 export default axios;
