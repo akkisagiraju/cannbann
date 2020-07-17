9801398af59b433adda3f6b91094305da710ba16
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-07-17 22:13:50 +0530

    sending auth token with the actual requests

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Container from '../styles/Container';
 import Button from '../styles/Button';
 import axios from '../config/axios';
+import useAuth from '../hooks/useAuth';
 
 interface BoardProps {
   realboard?: boolean;
@@ -38,9 +39,14 @@ const Home: React.FC = () => {
   const [addingBoard, setAddingBoard] = React.useState<boolean>(false);
   const [boardName, setBoardName] = React.useState<string>('');
   const [boards, setBoards] = React.useState<BoardObject[]>([]);
+  const { user } = useAuth();
 
   const getAllBoards = React.useCallback(async () => {
-    const response = await axios.get('/api/boards');
+    const response = await axios.get('/api/boards', {
+      headers: {
+        Authorization: 'Bearer ' + user.token
+      }
+    });
     console.log(response.data);
     setBoards(response.data);
   }, []);
@@ -50,7 +56,15 @@ const Home: React.FC = () => {
   }, [getAllBoards]);
 
   const createBoard = async () => {
-    const response = await axios.post('/api/boards', { title: boardName });
+    const response = await axios.post(
+      '/api/boards',
+      { title: boardName },
+      {
+        headers: {
+          Authorization: 'Bearer ' + user.token
+        }
+      }
+    );
     console.log(response);
     setAddingBoard(false);
     setBoardName('');
