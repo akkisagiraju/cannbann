b5bc7ba96ea2f412b9be42cf1806f649093c9950
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-07-18 22:50:42 +0530

    added a view for Board

---
+++ client/src/pages/Board.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Board: React.FC = () => {
+  return (
+    <div>
+      <h1>This page will have the lists of a board.</h1>
+    </div>
+  );
+};
+
+export default Board;
