960c27645c910918636bd915701b7e21f29aa3ca
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-07-24 18:09:22 +0530

    wrapped routes with Switch

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Route, BrowserRouter, Redirect } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
 import PrivateRoute from './PrivateRoute';
 import Home from '../pages/Home';
+import Board from '../pages/Board';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={SignIn} />
-      <Route path="/signup" component={SignUp} />
-      <PrivateRoute path="/home" component={Home} />
+      <Switch>
+        <Route path="/" exact component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <PrivateRoute path="/home" component={Home} />
+        <PrivateRoute path="/board" component={Board} />
+      </Switch>
     </BrowserRouter>
   );
 };
